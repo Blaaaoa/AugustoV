9d2eacd06703e0fb3ac990a78668b474dd5329f7
AugustoV2 <augustovarghese@gmail.com>
2024-11-11 04:50:57 +0530

    aa

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-        <span>Blaaa</span>
+        <span>AugustoV</span>
         <p className="text-slate-600">Have a Gooood Day.</p>
       </div>
     </footer>
